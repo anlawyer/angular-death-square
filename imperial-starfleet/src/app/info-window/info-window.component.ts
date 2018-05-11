b2e3f0378fd6023569624110ad514f0b3016a34e
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-10 18:23:40 -0700

    functional turrets with data from db

---
+++ imperial-starfleet/src/app/info-window/info-window.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { ActivatedRoute }   from '@angular/router';
+
+@Component({
+  selector: 'app-info-window',
+  templateUrl: './info-window.component.html',
+  styleUrls: ['./info-window.component.css']
+})
+export class InfoWindowComponent implements OnInit {
+
+  dataBanks: any;
+
+  constructor(
+    private http: Http,
+    private route: ActivatedRoute
+  ) { }
+
+  findTurret(turretNum) {
+    this.http.get('http://localhost:3000/api/turret/' + turretNum)
+    .toPromise()
+    .then(response => this.dataBanks = response.json());
+  }
+
+  ngOnInit() {
+    this.route.params.forEach( param => this.findTurret(param.id) )
+  }
+}
